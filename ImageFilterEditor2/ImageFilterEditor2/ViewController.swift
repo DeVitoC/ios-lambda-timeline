6066dfbe41f3c70360fa8f46589bc0ef8460839f
Christopher DeVito <christopher.devito@protonmail.com>
2020-05-05 10:37:04 -0400

    Created ImageFilterEditor2 project

---
+++ ImageFilterEditor2/ImageFilterEditor2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageFilterEditor2
+//
+//  Created by Christopher Devito on 5/5/20.
+//  Copyright © 2020 Christopher Devito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
